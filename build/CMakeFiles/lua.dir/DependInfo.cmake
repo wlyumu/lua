360788463b1c868ce88c3315dc1e52e3a185717a
aisimba <1546205823@qq.com>
2020-05-15 10:50:34 +0800

    lua test

--- build/CMakeFiles/lua.dir/DependInfo.cmake
+++ build/CMakeFiles/lua.dir/DependInfo.cmake
@@ -1,15 +1,53 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
+  "C"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/wl/Desktop/lua/main.cpp" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/main.cpp.obj"
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/wl/Desktop/lua/Lua/lapi.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lapi.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lauxlib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lauxlib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lbaselib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lbaselib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lbitlib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lbitlib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lcode.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lcode.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lcorolib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lcorolib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lctype.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lctype.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ldblib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ldblib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ldebug.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ldebug.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ldo.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ldo.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ldump.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ldump.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lfunc.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lfunc.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lgc.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lgc.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/linit.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/linit.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/liolib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/liolib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/llex.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/llex.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lmathlib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lmathlib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lmem.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lmem.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/loadlib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/loadlib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lobject.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lobject.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lopcodes.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lopcodes.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/loslib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/loslib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lparser.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lparser.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lstate.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lstate.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lstring.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lstring.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lstrlib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lstrlib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ltable.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ltable.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ltablib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ltablib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/ltm.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/ltm.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lundump.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lundump.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lutf8lib.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lutf8lib.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lvm.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lvm.c.obj"
+  "C:/Users/wl/Desktop/lua/Lua/lzio.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/Lua/lzio.c.obj"
+  "C:/Users/wl/Desktop/lua/luatest/lua_getfield_setfield.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/luatest/lua_getfield_setfield.c.obj"
+  "C:/Users/wl/Desktop/lua/luatest/lua_gettable_settable.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/luatest/lua_gettable_settable.c.obj"
+  "C:/Users/wl/Desktop/lua/luatest/lua_index.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/luatest/lua_index.c.obj"
+  "C:/Users/wl/Desktop/lua/main.c" "C:/Users/wl/Desktop/lua/build/CMakeFiles/lua.dir/main.c.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././Lua"
+  "../LUA_INC"
   )
 
 # Targets to which this target links.
